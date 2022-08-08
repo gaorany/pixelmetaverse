31ceb148a46b9b4662debb87446410b1afece53c
gaorany <gaoranycos@gmail.com>
2022-08-08 21:51:34 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    "no-multi-spaces": "off",
+    // allow async-await
+    'generator-star-spacing': 'off',
+    "no-async-promise-executor":'off'
+  }
+}
